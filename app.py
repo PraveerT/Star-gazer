f718075bf5e1915fe45459b9eb0757c09a382596
PraveerT <praveertowakel@yahoo.com>
2022-04-24 09:24:19 +0400

    App improvements

--- app.py
+++ app.py
@@ -9,22 +9,44 @@ import star_gazer
 
 from PIL import Image, ImageTk
 
+def read(choice,variable):
+    choice=variable
+    print (choice)
+
 class gui:
-    def __init__(self,data):
-        # Create an instance of TKinter Window or frame
-        self.data=data
-        self.window = tk.Tk()
+    def __init__(self):
         
-    def run(self):
+        # Create an instance of TKinter Window or frame
+        
+        self.window = tk.Tk()
+    def menu(self):
+        variable = tk.StringVar()
+        variable.set("Stars") # default value
+        w = tk.OptionMenu(self.window, variable, *self.staridlist,command=partial(read,variable))
+        w.grid(row=19,column=0)
+        
+    def run(self,data):
+
+        self.data=data
+        self.staridlist=[self.data[x]["id"] for x in range(len(self.data))]
+        
+
+        
         row=list(range(18))+list(range(18))
         col=[0]*18+[3]*18
+        
+        self.menu()
+        
+
         [tk.Label(text=str(x),wraplength=500, justify= tk.LEFT).grid(row =r, column =c) for x,r,c in zip(self.data[0],row,col)] 
         [tk.Label(text=str(x),wraplength=500, justify= tk.LEFT).grid(row =r, column =c+1) for x,r,c in zip(self.data[0].values(),row,col)] 
         self.window.mainloop()
+
+
         
     
 Stars=star_gazer.Group(r'C:\Users\prav\All_Projects\Other\Star_gazer\hygdata_v3.csv\hygdata_v3.csv')   
 Stars.data([2,3])
 
-instance=gui(Stars.data([2]))
-instance.run()
+instance=gui()
+instance.run(Stars.data(list(range(100))))
