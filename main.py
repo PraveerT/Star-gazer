3a8f435c558d0f4fab74ee84a6802374878cf92d
PraveerT <praveertowakel@yahoo.com>
2022-05-01 14:15:11 +0400

    clean up

--- main.py
+++ main.py
@@ -13,23 +13,23 @@ class GUI:
         self.win.wm_title("Star Gazer")
         self.categories1=catergories1
         self.categories1_keys=list(catergories1.keys())
-
+        #image
         self.tempimage=Image.open(r"C:\Users\prav\All_Projects\Other\Star_gazer\Images\Star_logo.png")
         self.finalimage=self.tempimage.resize((round(self.tempimage.size[0]*0.07),round(self.tempimage.size[1]*0.07)))
         self.image = ImageTk.PhotoImage(self.finalimage)
+        #geomery
         self.win.geometry("780x580")
         self.win.resizable(False, False)
-        self.data=data
-        self.textframebodytext="This database is a subset of the data in three major catalogs:the Hipparcos Catalog,the Yale Bright Star Catalog (5th Edition), and the Gliese Catalog of Nearby Stars (3rd Edition). "
         self.mainframeheight=406
         self.mainframewidth=600
         self.row=list(range(18))+list(range(18))
         self.col=[0]*18+[3]*18
-        self.variable = StringVar()
-        self.variable.set(0) # default value
-        self.mainframe=LabelFrame(self.win,text="Info",height=self.mainframeheight,width=self.mainframewidth)
-        self.mainframe.grid(column=1,row=1)
-        self.mainframe.grid_propagate(0)
+        self.padx=70
+        self.pady=40
+        #data for stars
+        self.data=data
+        #text entries and text frame
+        self.textframebodytext="This database is a subset of the data in three major catalogs:the Hipparcos Catalog,the Yale Bright Star Catalog (5th Edition), and the Gliese Catalog of Nearby Stars (3rd Edition). "
         self.textframe=LabelFrame(self.win, height=100,width=700)
         self.textframe.grid(column=0,row=0,columnspan=2)
         self.textframetitle=Label(self.textframe,text="Star Catalogue")
@@ -38,12 +38,14 @@ class GUI:
         self.textframebody.place(relx=.5, rely=.6,anchor= CENTER)
         self.textframebody=Label(self.textframe,image=self.image)
         self.textframebody.place(relx=0.01, rely=0)
-        self.padx=70
-        self.pady=40
-
+        #Mainframe
+        self.mainframe=LabelFrame(self.win,text="Info",height=self.mainframeheight,width=self.mainframewidth)
+        self.mainframe.grid(column=1,row=1)
+        self.mainframe.grid_propagate(0)
         self.attribute=[Label(self.mainframe,text=(str(x)).capitalize()+":",wraplength=500, justify= LEFT, font=('Helvetica', 8, 'bold')).grid(row =r, column =c,sticky='ew',padx= self.padx) for x,r,c in zip(self.data[0].keys(),self.row,self.col)] 
         self.values=[Label(self.mainframe,text=str(x),wraplength=500, justify= LEFT).grid(row =r, column =c+1,sticky='ew') for x,r,c in zip(self.data[0].values(),self.row,self.col)] 
 
+        #Listbox
         self.selectbox=LabelFrame(self.win,text="All stars")
         self.selectbox.grid(column=0,row=1)
         self.listbox = Listbox(self.selectbox,listvariable=StringVar(value=list(self.data.keys())),height=24,selectmode='extended')
@@ -52,16 +54,13 @@ class GUI:
         self.scrollbar = Scrollbar(self.selectbox,orient='vertical',command=self.listbox.yview)
         self.listbox['yscrollcommand'] = self.scrollbar.set
         self.scrollbar.grid(column=1,row=0,sticky='ns')
-        self.categories1default=self.categories1_keys[0]
+        #Option
         self.OptionVar= StringVar()
         # self.button_1= Button(self.win, text ="Hello",command=partial(self.UpdateMainFrame,1)).grid(row=1,column=1)
-        self.Options = OptionMenu(self.win, self.OptionVar,"Constellation", *self.categories1_keys,command=self.UpdateScrollbar).grid(row=3,column=0)
+        self.Options = OptionMenu(self.win, self.OptionVar,"Constellation", *self.categories1_keys,command=self.UpdateScrollbar).grid(row=3,column=0,pady=15)
         self.listbox.bind('<<ListboxSelect>>', self.UpdateMainFrame)
-    
+  
     def UpdateScrollbar(self,event):
-        self.selectbox.grid_forget()
-        self.selectbox=LabelFrame(self.win,text="All stars")
-        self.selectbox.grid(column=0,row=1)
         self.listbox = Listbox(self.selectbox,listvariable=StringVar(value=(self.categories1[str(self.OptionVar.get())])),height=24,selectmode='extended')
         self.scrollbar = Scrollbar(self.selectbox,orient='vertical',command=self.listbox.yview)
         self.listbox['yscrollcommand'] = self.scrollbar.set
@@ -71,10 +70,6 @@ class GUI:
 
     def UpdateMainFrame(self,event):
         self.choice=int(self.listbox.get(int(list(self.listbox.curselection())[0])))
-        self.mainframe.grid_forget()
-        self.mainframe=LabelFrame(self.win,text="Info",height=self.mainframeheight,width=self.mainframewidth)
-        self.mainframe.grid(column=1,row=1)
-        self.mainframe.grid_propagate(0)
         self.attribute=[Label(self.mainframe,text=(str(x)).capitalize()+":",wraplength=500, justify= LEFT, font=('Helvetica', 8, 'bold')).grid(row =r, column =c,sticky='ew',padx= self.padx) for x,r,c in zip(self.data[self.choice].keys(),self.row,self.col)] 
         self.values=[Label(self.mainframe,text=str(x),wraplength=500, justify= LEFT).grid(row =r, column =c+1,sticky='ew') for x,r,c in zip(self.data[self.choice].values(),self.row,self.col)] 
 
@@ -83,11 +78,5 @@ class GUI:
         self.win.mainloop()
 
 Stars=star_gazer.Group(r'C:\Users\prav\All_Projects\Other\Star_gazer\hygdata_v3.csv\hygdata_v3.csv')   
-Mystars=Stars.data()
-
-# print (Stars.stardict[0])
-
-# print (Stars.stardict[0])
-
 Mywindow=GUI(Stars.stardict,Stars.categoriescon)
 Mywindow.run()
