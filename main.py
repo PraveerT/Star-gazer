443504672449993d1943693bfaf37def53acee67
PraveerT <praveertowakel@yahoo.com>
2022-05-01 08:52:34 +0400

    LOGO

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
+from http.client import IM_USED
 import textwrap
 from tkinter import *
 from tkinter.ttk import *
 import numpy as np
 import star_gazer
 from functools import partial
+from PIL import Image, ImageTk
+
 class GUI:
     def __init__(self,menuitem,id):
         self.win=Tk()
+        self.win.wm_title("Star Gazer")
+        self.tempimage=Image.open(r"C:\Users\prav\All_Projects\Other\Star_gazer\Images\Star_logo.png")
+        self.finalimage=self.tempimage.resize((round(self.tempimage.size[0]*0.07),round(self.tempimage.size[1]*0.07)))
+        self.image = ImageTk.PhotoImage(self.finalimage)
         self.win.geometry("780x580")
         self.win.resizable(False, False)
         self.menuitem=menuitem
         self.id=id
         self.textframebodytext="This database is a subset of the data in three major catalogs:the Hipparcos Catalog,the Yale Bright Star Catalog (5th Edition), and the Gliese Catalog of Nearby Stars (3rd Edition). "
-        self.mainframeheight=400
+        self.mainframeheight=406
         self.mainframewidth=600
         self.row=list(range(18))+list(range(18))
         self.col=[0]*18+[3]*18
@@ -21,20 +28,22 @@ class GUI:
         self.mainframe=LabelFrame(self.win,text="Info",height=self.mainframeheight,width=self.mainframewidth)
         self.mainframe.grid(column=1,row=1)
         self.mainframe.grid_propagate(0)
-        self.textframe=LabelFrame(self.win, height=100,width=400)
+        self.textframe=LabelFrame(self.win, height=100,width=700)
         self.textframe.grid(column=0,row=0,columnspan=2)
         self.textframetitle=Label(self.textframe,text="Star Catalogue")
         self.textframetitle.place(relx=.5, rely=.2,anchor= CENTER)
         self.textframebody=Label(self.textframe,text=self.textframebodytext,wraplength=350)
-        self.textframebody.place(relx=.5, rely=.7,anchor= CENTER)
+        self.textframebody.place(relx=.5, rely=.6,anchor= CENTER)
+        self.textframebody=Label(self.textframe,image=self.image)
+        self.textframebody.place(relx=0.01, rely=0)
         self.padx=70
         self.pady=40
-        self.attribute=[Label(self.mainframe,text=str(x),wraplength=500, justify= LEFT).grid(row =r, column =c,sticky='ew',padx= self.padx) for x,r,c in zip(self.menuitem[0],self.row,self.col)] 
+        self.attribute=[Label(self.mainframe,text=(str(x)).capitalize()+":",wraplength=500, justify= LEFT, font=('Helvetica', 8, 'bold')).grid(row =r, column =c,sticky='ew',padx= self.padx) for x,r,c in zip(self.menuitem[0],self.row,self.col)] 
         self.values=[Label(self.mainframe,text=str(x),wraplength=500, justify= LEFT).grid(row =r, column =c+1,sticky='ew') for x,r,c in zip(self.menuitem[0].values(),self.row,self.col)] 
 
         self.selectbox=LabelFrame(self.win,text="All stars")
         self.selectbox.grid(column=0,row=1)
-        self.listbox = Listbox(self.selectbox,listvariable=StringVar(value=self.id),height=20,selectmode='extended')
+        self.listbox = Listbox(self.selectbox,listvariable=StringVar(value=self.id),height=24,selectmode='extended')
         self.listbox.grid(column=0,row=0,sticky='nwes')
         
         self.scrollbar = Scrollbar(self.selectbox,orient='vertical',command=self.listbox.yview)
@@ -52,7 +61,7 @@ class GUI:
         self.mainframe=LabelFrame(self.win,text="Info",height=self.mainframeheight,width=self.mainframewidth)
         self.mainframe.grid(column=1,row=1)
         self.mainframe.grid_propagate(0)
-        self.attribute=[Label(self.mainframe,text=str(x),wraplength=500, justify= LEFT).grid(row =r, column =c,sticky='ew',padx= self.padx) for x,r,c in zip(self.menuitem[self.choice],self.row,self.col)] 
+        self.attribute=[Label(self.mainframe,text=str(x).capitalize()+":",wraplength=500, justify= LEFT, font=('Helvetica', 8, 'bold')).grid(row =r, column =c,sticky='ew',padx= self.padx) for x,r,c in zip(self.menuitem[self.choice],self.row,self.col)] 
         self.values=[Label(self.mainframe,text=str(x),wraplength=500, justify= LEFT).grid(row =r, column =c+1,sticky='ew') for x,r,c in zip(self.menuitem[self.choice].values(),self.row,self.col)] 
 
 
